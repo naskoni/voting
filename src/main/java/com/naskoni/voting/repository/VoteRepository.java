c1d9671ef339394ab16e1b8617a69269b3f445f6
Atanas Atanasov <naskoni@users.noreply.github.com>
2020-07-09 12:24:23 +0300

    Update VoteRepository.java

--- src/main/java/com/naskoni/voting/repository/VoteRepository.java
+++ src/main/java/com/naskoni/voting/repository/VoteRepository.java
@@ -1,6 +1,7 @@
 package com.naskoni.voting.repository;
 
 import com.naskoni.voting.entity.BlogPostVote;
+import org.hibernate.StaleObjectStateException;
 import org.springframework.dao.CannotAcquireLockException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -15,11 +16,18 @@ import java.util.Optional;
 public interface VoteRepository extends JpaRepository<BlogPostVote, Long> {
 
   /**
-   * The chosen DB lock strategy is OPTIMISTIC_FORCE_INCREMENT. PESSIMISTIC_READ was tested
+   * Retrieves a BlogPostVote entity by the id of its BlogPost.
+   *
+   * <p>The chosen DB lock strategy is OPTIMISTIC_FORCE_INCREMENT. PESSIMISTIC_READ was tested
    * succesfully with H2 but with MS SQL Server was failing immediately with {@link
    * CannotAcquireLockException} and there was not possible to tune the server to wait for acquiring
    * a lock with this: {@literal @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout",
    * value = "3000")})}
+   *
+   * @param id must not be {@literal null}.
+   * @return the entity with the given id or {@literal Optional#empty()} if none found.
+   * @throws IllegalArgumentException if {@literal id} is {@literal null}.
+   * @throws StaleObjectStateException if entity was updated or deleted by another transaction.
    */
   @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
   Optional<BlogPostVote> findByBlogPostId(Long id);
